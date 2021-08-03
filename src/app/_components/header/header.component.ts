66a0271dcd5eac8205c4ec709da309516cb91fea
ddomi2048 <ddomi2048@gmail.com>
2021-08-03 20:09:47 +0200

    idk

--- src/app/_components/header/header.component.ts
+++ src/app/_components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+  constructor(private route: Router) { }
+
+
+  url: string = ''
 
   ngOnInit(): void {
+      this.display();
+  }
+
+  display() {
+    setTimeout(() => {
+      this.url = this.route.url;
+    }, 10);
+    if (this.url !== '/home') {
+      return true;
+    } else {
+      return false;
+    }
   }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './_components/header/header.component';
+import { HomeComponent } from './_components/home/home.component';
+import { GameComponent } from './game/game.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    HomeComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
